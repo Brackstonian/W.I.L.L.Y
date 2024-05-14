b29a3e21a07ee1f2f6a35375c48651da40ed2eba
devbrackstone <dev.brackstone@gmail.com>
2024-05-14 23:42:43 +0100

    share modal update

--- src/renderer/components/share-modal.js
+++ src/renderer/components/share-modal.js
@@ -4,8 +4,12 @@ import { cursorSetup } from './globals/cursor';
 document.addEventListener('DOMContentLoaded', () => {
     cursorSetup()
     const copyToClipboard = document.getElementById('copyToClipboard');
+    const copyToClipboardUrl = document.getElementById('copyToClipboardUrl');
 
     copyToClipboard.addEventListener('click', () => {
         navigator.clipboard.writeText(copyToClipboard.dataset.shareid);
+    })
+    copyToClipboardUrl.addEventListener('click', () => {
+        navigator.clipboard.writeText(copyToClipboardUrl.dataset.shareurl);
     })
 });
