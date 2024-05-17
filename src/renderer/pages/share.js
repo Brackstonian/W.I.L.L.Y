2bca0340eae42b9329886d8c16ad11b7cce9644a
devbrackstone <dev.brackstone@gmail.com>
2024-05-17 20:46:04 +0100

    Log Copy Button

--- src/renderer/pages/share.js
+++ src/renderer/pages/share.js
@@ -6,7 +6,7 @@ function logPeerId(peerId) {
     addLog(`To share this connection, use the following link:`, '.app-main-log--ps');
     addLog(`<a href="https://w-i-l-l-y-web.onrender.com/view/${peerId}" target="_blank">https://w-i-l-l-y-web.onrender.com/view/${peerId}</a>`, '.app-main-log--ps', true);
 
-    document.querySelector('.log__button').addEventListener('click', () => {
+    document.getElementById('copyToClipboard').addEventListener('click', () => {
         const fullText = document.querySelector('.app-main-log--ps').innerText;
         const lines = fullText.split('\n');
         let modifiedText = lines.slice(2).join('\n');
